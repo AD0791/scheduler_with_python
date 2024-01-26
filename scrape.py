669d649bb81183ca394edc123849bd45dddb006d
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2024-01-26 16:20:19 -0500

    every half minutes

--- scrape.py
+++ scrape.py
@@ -1,11 +1,32 @@
 from requests import get
 from bs4 import BeautifulSoup
 from icecream import ic
+from schedule import run_pending, every, repeat
+import time
 
-URL= 'https://news.ycombinator.com/'
+@repeat(every(0.5).minutes,  n=3)
+def get_articles_from_ycombinator(n:int)->list[dict]:
+    URL= 'https://news.ycombinator.com/'
+    res = get(URL)
+    soup = BeautifulSoup(res.content,'html.parser')
+    data = list()
+    links = soup.select('.titleline > a')
+    scores = soup.select('.score')
+    for link, score in zip(links , scores):
+        details = dict()
+        details['article_name'] = link.text
+        details['article_url'] = link.attrs['href']
+        details['article_score'] = int(score.text.split()[0])
+        data.append(details)
+    data = sorted(data,key=lambda x: x['article_score'], reverse=True)[:n]
+    ic('--------------------')
+    ic(data)
+    return data
 
-res = get(URL)
-soup = BeautifulSoup(res.content,'html.parser')
 
-links = soup.select('.titleline > a')
-ic(links)
+
+####### schedulers
+
+while True:
+    run_pending()
+    time.sleep(1)
